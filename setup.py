e4d3bf17268796255ababfab2a5a908a6b338fc1
bnwlkr <bnwlkr@icloud.com>
2020-09-12 18:02:40 -0700

    Leonidas beta deployed to server

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="leonidas",
+    version="0.0.1",
+    author="bnwlkr",
+    description="UBC Discord's Loyal Protector",
+    url="https://github.com/bnwlkr/Leonidas",
+    packages=setuptools.find_packages(),
+    scripts=['runleonidas.py'],
+    install_requires=[
+        'discord',
+        'python-dotenv',
+        'aiohttp'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
